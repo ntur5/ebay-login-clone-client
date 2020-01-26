1c4906398533eef0b4fea210ab7dd4c660fc06de
pingt9*t <pressntr@gmail.com>
2020-01-26 02:41:53 -0700

    almost complete with styling of the front end

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
-import Logo from '../../static/assets/images/ebay-logo.jpg'
+import logo from '../../static/assets/images/ebay-logo.jpg'
+import adChoice from '../../static/assets/images/adChoice.png'
+import checkBox from '../../static/assets/images/checkbox.png'
 
 const divStyle = {
    
@@ -32,7 +34,7 @@ export default class Login extends Component {
             <div className="header">
                 <a href="https://www.ebay.com/">
                     <img style={divStyle}
-                        src="../../static/assets/images/ebay-logo.jpg"
+                        src={logo}
                         // src="https://ir.ebaystatic.com/rs/v/fxxj3ttftm5ltcqnto1o4baovyl.png" 
                         alt="eBay Logo">
                     </img>
@@ -41,7 +43,7 @@ export default class Login extends Component {
             <div className="welcome-container">
                 <div className="greeting">
                     <h1 className="heading">Hello</h1>
-                    <div className="sub-heading">sign in to eBay or <a href="https://reg.ebay.com/reg/PartialReg?siteid=0&co_partnerId=0&UsingSSL=1&rv4=1&ru=https%3A%2F%2Fwww.ebay.com%2F&signInUrl=https%3A%2F%2Fwww.ebay.com%2Fsignin%3Fsgn%3Dreg%26siteid%3D0%26co_partnerId%3D0%26UsingSSL%3D1%26rv4%3D1%26ru%3Dhttps%253A%252F%252Fwww.ebay.com%252F">create an account</a></div>
+                    <div className="sub-heading">Sign in to eBay or <a href="https://reg.ebay.com/reg/PartialReg?siteid=0&co_partnerId=0&UsingSSL=1&rv4=1&ru=https%3A%2F%2Fwww.ebay.com%2F&signInUrl=https%3A%2F%2Fwww.ebay.com%2Fsignin%3Fsgn%3Dreg%26siteid%3D0%26co_partnerId%3D0%26UsingSSL%3D1%26rv4%3D1%26ru%3Dhttps%253A%252F%252Fwww.ebay.com%252F">create an account</a></div>
                 </div>
             </div>
             <div className="signin-container">
@@ -81,8 +83,8 @@ export default class Login extends Component {
                     <div className="separator-bottom animate"></div>
                 </div>
                 <div className="signin-buttons">
-                    <button className="fb-btn"><img src="https://ir.ebaystatic.com/rs/c/sgninui-src-static-images-FB-f-Logo__white_29-Nm8L0bDZ.png" />Sign in with Facebook</button>
-                    <button className="google-btn"><img src="https://ir.ebaystatic.com/rs/c/sgninui-src-static-images-google-logo-icon-PNG-Transparent-Background-Z_TFsqo3.png" />Sign in with Google</button>
+                    <button className="fb-btn"><img className="facebook-image" src="https://ir.ebaystatic.com/rs/c/sgninui-src-static-images-FB-f-Logo__white_29-Nm8L0bDZ.png" />Sign in with Facebook</button>
+                    <button className="google-btn"><img className="google-image" src="https://ir.ebaystatic.com/rs/c/sgninui-src-static-images-google-logo-icon-PNG-Transparent-Background-Z_TFsqo3.png" />Sign in with Google</button>
                     {/* <button className="google-btn">Sign in with Google</button> */}
                 </div>
                 {/* <div className="social-buttons-container">
@@ -92,18 +94,17 @@ export default class Login extends Component {
             </div>
             <div className="stay-signed-in-container">
                 <div className="checkbox-container">
-                    <input id="csi" className="checkbox-control" type="checkbox" checked="" value="1">
-
-                    </input>
+                    <img src={checkBox} />
+                    {/* <input id="csi" className="checkbox-control" type="checkbox" checked="" value="1" /> */}
                     <label for="csi">Stay signed in</label>
                 </div>
                 <div className="privacy-text">
-                    <div>
+                    <div className="privacy-text-divs">
                         Using a public or shared device?
                     </div>
-                    <div>
+                    <div className="privacy-text-divs">
                         Uncheck to protect your account. 
-                    <a href="#" className="learn-more-link" role="button" aria-label="Learn more about stay signed in." >Learn more</a>
+                    <a href="#" className="learn-more-link" role="button" aria-label="Learn more about stay signed in." >&nbsp;Learn more</a>
                     </div>
                 </div>
             </div>
@@ -114,9 +115,9 @@ export default class Login extends Component {
                     <a href="https://pages.ebay.com/help/policies/user-agreement.html">User Agreement,</a>
                     <a href="https://pages.ebay.com/help/policies/privacy-policy.html">Privacy,</a>
                     <a href="https://pages.ebay.com/help/account/cookies-web-beacons.html">Cookies,</a>
-                    and <a href="https://www.ebay.com/adchoice">AdChoice</a>
+                    and <a href="https://www.ebay.com/adchoice">AdChoice<img className="adChoice-image" src={adChoice} alt="adChoice" /></a>
                 </div>
-                <img src="https://ir.ebaystatic.com/rs/v/f5uxsy10bmz05dtrtrqybl5qquv.png"></img>
+                <img className="norton-image" src="https://ir.ebaystatic.com/rs/v/f5uxsy10bmz05dtrtrqybl5qquv.png"></img>
             </div>
         </div>
         )
